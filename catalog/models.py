c71ad9a0871c163e4a2f98ee6f4d3a1df8bda084
Sebas Moreno <samorenoa@gmail.com>
2022-11-10 23:12:58 -0500

    Form de create movies

--- catalog/models.py
+++ catalog/models.py
@@ -1,29 +1,52 @@
 # Create your models here.
+from django.contrib.auth.models import User
+from django.core.validators import MinLengthValidator
 from django.db import models
 from ckeditor.fields import RichTextField
 
 class Movie(models.Model):
     name = models.CharField(max_length=35)
     category = models.CharField(max_length=50)
-    rate = models.IntegerField()
+    rate = models.IntegerField(null=False, blank=False)
     review = RichTextField(null=True, blank=True)
+    image = models.ImageField(upload_to='movie', null =True, blank=True)
+    owner = models.ForeignKey(User, on_delete=models.CASCADE)
+    comments = models.ManyToManyField(
+        User, through="Comment", related_name="comments_owned"
+    )
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
     #img = models.ImageField(Upload_to='img') --> PREGUNTAR
-        
+
+    class Meta:
+        unique_together = (
+            "name",
+            "category",
+        )
+        ordering = ["-created_at"]
+
+class Comment(models.Model):
+    text = models.TextField(
+        validators=[
+            MinLengthValidator(10, "El comentario debe ser mayor de 10 caracteres")
+        ]
+    )
+    movie = models.ForeignKey(Movie, on_delete=models.CASCADE)
+    owner = models.ForeignKey(User, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
     def __str__(self):
-        return f"{self.name} | {self.category} | {self.rate}"
+     return f"{self.name} | {self.category} | {self.rate}"
 
 class Serie(models.Model):
     name = models.CharField(max_length=35)
     category = models.CharField(max_length=50)
-    seasons = models.IntegerField()
-    rate = models.IntegerField()
+    seasons = models.IntegerField(null=False, blank=False)
+    rate = models.IntegerField(null=False, blank=False)
     review = RichTextField(null=True, blank=True)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
     #img = models.ImageField(Upload_to='img') --> PREGUNTAR
-        
     def __str__(self):
-        return f"{self.name} | {self.category} | {self.seasons}"
-# Create your models here.
+        return f"{self.name} | {self.category} | {self.seasons}"   
